774f9afddac2a9bcf8189d476a77d4ab0ebe2927
Volodymyr <klepalskyi.v@gmail.com>
2025-05-23 09:04:30 +0300

    add 3-5 js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+
+  colorSpan.textContent = newColor;
+});
